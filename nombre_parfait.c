b8851c3c959c88fe1e155f3e890b08396a01b3b1
salimdaouas <79147624+salimdaouas@users.noreply.github.com>
2021-10-15 11:05:21 +0200

    Add files via upload

---
+++ nombre_parfait.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+
+int main {
+
+int a;
+    printf("saisissez un chiffre:\n");
+    scanf(%d,"&a");
+for (i=a-1;i!=1;i--){
+    if (a%i=0){
+        printf("Le nombre %d n'est pas parfait",&a);
+    }
+    else{
+    printf("Le nombre %d est pas parfait",&a);
+    }
+    }
+return 0;
+}
+
+
+
+
+
+
+
+
+}
